cbe6f7cf4a0efb124c9febc8db37bb7c1e07c285
ZolotoY Pers <samirazimov488@gmail.com>
2025-10-05 18:56:01 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+// Program.cs
+
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+await builder.Build().RunAsync();
